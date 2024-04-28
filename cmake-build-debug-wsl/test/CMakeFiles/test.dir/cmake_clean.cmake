c6f26d7a84fde935d89916065dce22e12fdd56ed
cyf <505224730@qq.com>
2024-04-28 10:25:11 +0800

    Dockefile 增加 ca-certificates

---
+++ cmake-build-debug-wsl/test/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/src/TriPsReader.cpp.o"
+  "CMakeFiles/test.dir/src/TriPsReader.cpp.o.d"
+  "CMakeFiles/test.dir/src/mongoose.c.o"
+  "CMakeFiles/test.dir/src/mongoose.c.o.d"
+  "CMakeFiles/test.dir/src/test.cpp.o"
+  "CMakeFiles/test.dir/src/test.cpp.o.d"
+  "test"
+  "test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-release/sdk/CMakeFiles/sdk.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sdk.dir/src/core_api.cpp.o"
+  "CMakeFiles/sdk.dir/src/core_api.cpp.o.d"
+  "CMakeFiles/sdk.dir/src/easylogging++.cc.o"
+  "CMakeFiles/sdk.dir/src/easylogging++.cc.o.d"
+  "CMakeFiles/sdk.dir/src/globalvar.cpp.o"
+  "CMakeFiles/sdk.dir/src/globalvar.cpp.o.d"
+  "CMakeFiles/sdk.dir/src/tri_gmssl.cpp.o"
+  "CMakeFiles/sdk.dir/src/tri_gmssl.cpp.o.d"
+  "CMakeFiles/sdk.dir/src/tri_nal.cpp.o"
+  "CMakeFiles/sdk.dir/src/tri_nal.cpp.o.d"
+  "CMakeFiles/sdk.dir/src/tri_skf_api.cpp.o"
+  "CMakeFiles/sdk.dir/src/tri_skf_api.cpp.o.d"
+  "CMakeFiles/sdk.dir/src/tri_util.cpp.o"
+  "CMakeFiles/sdk.dir/src/tri_util.cpp.o.d"
+  "libsdk.pdb"
+  "libsdk.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sdk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
